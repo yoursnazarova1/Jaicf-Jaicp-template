4f877fad088306e74c33995855bf072da072f032
a.nazarova <a.nazarova@just-ai.com>
2023-08-02 14:14:50 +0300

    changing

--- src/main/kotlin/com/justai/jaicf/template/connections/JaicpServer.kt
+++ src/main/kotlin/com/justai/jaicf/template/connections/JaicpServer.kt
@@ -14,7 +14,7 @@ fun main() {
         channels = listOf(
             ChatApiChannel,
             ChatWidgetChannel,
-            TelephonyChannel
+            TelephonyChannel,
         ), url = "https://lts-ha01.lo.test-ai.net/chatadapter"
     ).start(wait = true)
 }
